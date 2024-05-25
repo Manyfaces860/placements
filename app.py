16a365d1c6ffda4a414b07bf825a26e9fc5bbd2f
Manyfaces860 <127599665+Manyfaces860@users.noreply.github.com>
2024-05-25 15:36:57 +0530

    new commit

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 import streamlit as st
 import pickle
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("./placement.csv")
-df = df.drop("Unnamed: 0", axis=1)
-X = df.iloc[:, 0:2]
-Y = df.iloc[:, -1]
-x_train , x_test , y_train , y_test = train_test_split(X, Y , test_size=0.1,train_size=0.9)
-scaler = StandardScaler()
-scaler.fit_transform(x_train)
+
+scaler = pickle.load(open('scaler.pkl' , 'rb'))
 
 model = pickle.load(open('model.pkl' , 'rb'))
 
